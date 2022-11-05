1d163238c1ddfc25ed381f11bad756abd8c71f97
Ivan Cardozo <ivancardozo11@hotmail.com>
2022-11-05 08:23:50 +0100

    edit the date object and create button edition message Editing and save changes

--- js/app.js
+++ js/app.js
@@ -11,6 +11,9 @@ const  formulario = document.querySelector('#nueva-cita');
 const  contenedorCitas = document.querySelector('#citas');
 
 
+let editando;
+
+
 class Citas{
     constructor(){
         this.citas = []
@@ -18,7 +21,6 @@ class Citas{
 
     agregarCita(cita){
         this.citas = [...this.citas, cita];
-        console.log(this.citas);
     }
     eliminarCita(id){
         this.citas = this.citas.filter(cita => cita.id !== id);
@@ -95,6 +97,11 @@ class UI{
             btnELiminar.innerHTML = 'Eliminar <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" stroke-width="1.5" stroke="currentColor" class="w-6 h-6"><path stroke-linecap="round" stroke-linejoin="round" d="M9.75 9.75l4.5 4.5m0-4.5l-4.5 4.5M21 12a9 9 0 11-18 0 9 9 0 0118 0z" /></svg>';
             btnELiminar.onclick = () => eliminarCita(id);
 
+            //Añade un boton para editar
+            const btnEditar = document.createElement('button');
+            btnEditar.classList.add('btn', 'btn-info');
+            btnEditar.innerHTML = 'Editar <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" stroke-width="1.5" stroke="currentColor" class="w-6 h-6"><path stroke-linecap="round" stroke-linejoin="round" d="M16.862 4.487l1.687-1.688a1.875 1.875 0 112.652 2.652L6.832 19.82a4.5 4.5 0 01-1.897 1.13l-2.685.8.8-2.685a4.5 4.5 0 011.13-1.897L16.863 4.487zm0 0L19.5 7.125" /></svg>';
+            btnEditar.onclick = () => cargarEdicion(cita);
 
             //Agregar los parrafos al divCita
             divCita.appendChild(mascotaParrafo);
@@ -104,6 +111,7 @@ class UI{
             divCita.appendChild(horaParrafo);
             divCita.appendChild(sintomasParrafo);
             divCita.appendChild(btnELiminar);
+            divCita.appendChild(btnEditar);
 
             //Agregar las citas al HTML
             contenedorCitas.appendChild(divCita);
@@ -161,12 +169,22 @@ function nuevaCita(e){
         return;
     }
 
-    // Generar un id unico
-    citaObj.id = Date.now();
+    if (editando) {
+        ui.imprimirAlerta('Editado correctamente');
 
+        //Pasar el objeto de la cita a edicion
+        formulario.querySelector('button[type="submit"]').textContent = 'Crear cita';
+        editando = false;
+    }else{
+        // Generar un id unico
+        citaObj.id = Date.now();
 
-    //Creando una nueva cita
-    administrarCitas.agregarCita({...citaObj});
+        //Creando una nueva cita
+        administrarCitas.agregarCita({...citaObj});
+
+        ui.imprimirAlerta('Se agregó correctamente');
+    }
+
 
     //reiniciar el objeto para la validacion
 
@@ -198,4 +216,29 @@ function eliminarCita(id) {
 
     //Refrescar las citas
     ui.imprimirCitas(administrarCitas);
+}
+
+//Carga los datos y el modo edicion
+function cargarEdicion(cita) {
+    const { mascota, propietario, telefono, fecha, hora, sintomas, id }= cita;
+
+    //Llenar los inputs
+    mascotaInput.value = mascota;
+    propietarioInput.value = propietario;
+    telefonoInput.value = telefono;
+    fechaInput.value = fecha;
+    horaInput.value = hora;
+    sintomasInput.value = sintomas;
+
+    //Cambia el texto de la barra de crear cita
+    citaObj.mascota = mascota;
+    citaObj.propietario = propietario;
+    citaObj.telefono = telefono;
+    citaObj.fecha = fecha;
+    citaObj.hora = hora;
+    citaObj.sintomas = sintomas;
+    citaObj.id = id;
+    formulario.querySelector('button[type="submit"]').textContent = 'Guardar cambios';
+
+    editando = true;
 }
